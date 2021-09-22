2fe8b0ad79fff7fb3f78a7dcc67c931e0e00544b
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-09-21 23:15:50 -0500

    ajustes sql y modelo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,16 +10,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:Thispassword123*@l
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Book(db.Model):
-    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+class Card(db.Model):
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    state = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
         return "<Title: {}>".format(self.title)
-
-@app.route("/", methods=["GET", "POST"])
+    
+@app.route("/api/card", methods=["GET"])
 def home():
-    books = None
-    books = Book.query.all()
+    cards = Card.query.all()
+    return jsonify(cards)
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE card(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    state boolean not null
+)
