3a30e9dae8595181db0dc02a3239af39ac7a26e6
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-09-21 20:42:52 -0500

    avance

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-# save this as app.py
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:Thispassword123*@localhost:3399/to_do'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    books = None
+    if request.form:
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
