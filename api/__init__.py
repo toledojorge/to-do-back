abc2b69cb253f9b8d70b6d356aa42ebbc06e97a8
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-09-26 21:12:27 -0500

    ajuste api blueprint

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,4 @@
 from flask import Blueprint
-from flask import jsonify
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
-
-
-@api_bp.route('/test', methods=['GET'])
-def listar():
-    return jsonify("test")
+from api import card_api
--- api/card_api.py
+++ api/card_api.py
@@ -1,8 +1,6 @@
 from flask import jsonify
-from flask import Blueprint
+from api import api_bp
 
-card_bp = Blueprint('card', __name__, url_prefix='/api')
-
-@card_bp.route('/test', methods=['GET'])
+@api_bp.route('/test', methods=['GET'])
 def listar():
     return jsonify("test")
