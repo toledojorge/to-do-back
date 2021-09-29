296fdb08f538a9f51c02a186da9a4195a8902879
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-09-29 00:16:05 -0500

    ajustes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from app import run
 
 if __name__ == '__main__':    
-    run("config/local.py")
+    app = run("config/local.py")
--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,5 @@ def run(config):
     app.register_blueprint(api_bp)
     db.init_app(app)
     
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    return app
