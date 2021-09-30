2cab4dc6a8adf6615473bd468fbd179c81059892
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-09-29 20:56:10 -0500

    Ajuste script database.sql

--- database/database.sql
+++ database/database.sql
@@ -3,12 +3,12 @@ CREATE TABLE card(
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
     state boolean not null
-)
+);
 
 
  CREATE TABLE person(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    email UNIQUE VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL
- )
+ );
