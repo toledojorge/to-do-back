97e795c384742002450cefdcddf78822fd2c7567
Jorge Armando Toledo - Ceiba Software <jorge.toledo@ceiba.com.co>
2021-09-29 21:00:20 -0500

    foreign key person in card

--- database/database.sql
+++ database/database.sql
@@ -1,14 +1,15 @@
-CREATE TABLE card(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    description TEXT NOT NULL,
-    state boolean not null
-);
-
-
  CREATE TABLE person(
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL
  );
+
+ CREATE TABLE card(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    state boolean not null,
+    person_id INT NOT NULL,
+        FOREIGN KEY (person_id) REFERENCES person(id)
+);
